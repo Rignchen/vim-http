4e0ffc3b7995e5d12a26cf8454ca7ef4b8a51bc6
Rignchen <rinchendemort@gmail.com>
2024-05-08 16:27:12 +0200

    feat: give the ability to chose how the output if formated

--- autoload/temp.py
+++ autoload/temp.py
@@ -20,12 +20,12 @@ class http_request:
         # parse the instructions
         parced_instructions = {}
         for i in self.instructions:
-            if not i.startswith(("@name", "@no-log", "@no-output", "@html", "@file-format", "@timeout")):
+            if not i.startswith(("@name", "@no-log", "@no-output", "@html", "@file-format", "@output-format", "@file-extension", "@timeout")):
                 raise ValueError(f"Unknown instruction: {i}")
             parced_instructions[i.split(' ')[0]] = i.split(' ')[1] if ' ' in i else True
 
         try:
-            response = requests.request(self.method, self.url, headers=self.headers, data=self.body,  timeout=int(parced_instructions.get("@timeout", 10)))
+            response = requests.request(self.method, self.url, headers=self.headers, data=self.body, timeout=int(parced_instructions.get("@timeout", 10)))
         except requests.exceptions.ConnectTimeout:
             return http_response(f"{http_color.red}Connection Timeout{http_color.reset}", 408, {}, parced_instructions)
         except requests.exceptions.ConnectionError:
@@ -45,7 +45,7 @@ class http_color:
     black = "\033[30m"
     gray = "\033[90m"
     bold = "\033[1m"
-    
+
 class http_response:
     def __init__(self, text: str, code: int, headers: dict[str, str], instructions: dict[str, str|bool]):
         self.text = text
@@ -56,28 +56,29 @@ class http_response:
         # ensure you're supposed to write in a file
         if self.instructions.get("@no-log", False):
             return
-        
+
         # get the name of the file
         name = f".vim_http/{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{self.instructions.get('@name', '')}_{self.code}"
 
         # get the file format
-        format = '.' + self.instructions.get("@file-format", "log")
+        format = '.' + self.instructions.get("@file-extension", "log")
 
         # ensure the file doesn't already exist
         os.makedirs(os.path.dirname(f"{name}.{format}"), exist_ok=True)
         if os.path.exists(f"{name}.{format}"):
             count = 0
-            while os.path.exists(f"{name}({count}).{format}"):count += 1
+            while os.path.exists(f"{name}({count}).{format}"):
+                count += 1
             name = f"{name}({count})"
-        
+
         # write the response in the file
         with open(f"{name}.{format}", "w") as file:
-            file.write(self.text)
+            file.write(self.instructions.get("@file-format", "%c\n%h\n\n%t").replace("%n", self.instructions.get('@name', '')).replace("%c", str(self.code)).replace("%h", self.headers).replace("%t", self.text))
     def display(self):
         # ensure you're supposed to display the response
         if self.instructions.get("@no-output", False):
             return
-        
+
         # remove html balises if needed
         if self.instructions.get("@html", False):
             # remove comments
@@ -96,23 +97,23 @@ class http_response:
             # change the html special characters
             self.text = http_htmlSpecialCharacters(self.text)
 
-        name = f"{http_color.purple}{self.instructions['@name']}{http_color.reset}\n" if '@name' in self.instructions else ""
+        name = f"{http_color.purple}{self.instructions['@name']}{http_color.reset}" if '@name' in self.instructions else ""
 
         if self.code == 200:          code = ""
-        elif 201 <= self.code <= 299: code = f"{http_color.green }{http_color.bold}{self.code}{http_color.reset}\n"
-        elif 300 <= self.code <= 399: code = f"{http_color.yellow}{http_color.bold}{self.code}{http_color.reset}\n"
-        elif 400 <= self.code <= 499: code = f"{http_color.purple}{http_color.bold}{self.code}{http_color.reset}\n"
-        elif 500 <= self.code <= 599: code = f"{http_color.red   }{http_color.bold}{self.code}{http_color.reset}\n"
-        else:                         code = f"{http_color.blue  }{http_color.bold}{self.code}{http_color.reset}\n"
-        
+        elif 201 <= self.code <= 299: code = f"{http_color.green }{http_color.bold}{self.code}{http_color.reset}"
+        elif 300 <= self.code <= 399: code = f"{http_color.yellow}{http_color.bold}{self.code}{http_color.reset}"
+        elif 400 <= self.code <= 499: code = f"{http_color.purple}{http_color.bold}{self.code}{http_color.reset}"
+        elif 500 <= self.code <= 599: code = f"{http_color.red   }{http_color.bold}{self.code}{http_color.reset}"
+        else:                         code = f"{http_color.blue  }{http_color.bold}{self.code}{http_color.reset}"
+
         headers = f"{http_color.gray}{self.headers}{http_color.reset}\n" if self.headers != "" else ""
 
-        http_print(f"{name}{code}{headers}\n{self.text}")
+        http_print(self.instructions.get("@output-format", "%n\n%c\n%h\n\n%t").replace("%n", name).replace("%c", code).replace("%h", headers).replace("%t", self.text))
 
 def http_print(text: str):
     print(text)
     return
-    vim.command(f"new")
+    vim.command("new")
     vim.command(f"normal! i{text}\n")
 
 def http_help():
@@ -137,7 +138,9 @@ List of available instructions:
     - @no-log: by default the plugin store the response in a log file at the root of the project. This instruction disable this behavior
     - @no-output: by default the plugin display the response in a new buffer. This instruction disable this behavior
     - @html: remove every html balises from the response before displaying it in the buffer, the log file is not affecte
-    - @file-format <format>: the log file will be named with the specified format
+    - @file-format <format>: the log file will be written respecting the specified format (%t -> response's text, %c -> exit code, %h -> headers and %n -> name. Default is %c\n%h\n\n%t)
+    - @output-format <format>: the response output will be written respecting the specified format (%t -> response's text, %c -> exit code, %h -> headers and %n -> name. Default is %n\n%c\n%h\n\n%t)
+    - @file-extension <extension>: the log file will use the specified extension
     - @timeout <seconds>: set the timeout for the request, by default it's 10 seconds
 
 Example:
@@ -175,7 +178,7 @@ def http_parseRequest(request: list[str]):
     if len(request) == 0:
         raise ValueError("Request cannot be empty")
     output = http_request()
-        
+
     # remove leading and trailing whitespaces
     for n in range(len(request)):
         if re.match('^\s*#', request[n]): # comments
@@ -186,7 +189,7 @@ def http_parseRequest(request: list[str]):
             break # we have to keep the whole body intact so we can't remove leading and trailing whitespaces
 
     # get instructions, remove other comments
-    for i in range(n-1,-1,-1):
+    for i in range(n - 1, -1, -1):
         if request[i].startswith("#"):
             if re.match('^#{3,} {0,}[a-zA-Z0-9_\-]{1,}$',request[i]):
                 request[i] = re.sub('^#{3,} {0,}','#@name ',request[i], flags=re.DOTALL)
@@ -195,7 +198,7 @@ def http_parseRequest(request: list[str]):
                 output.instructions.insert(0,request.pop(i)[1:])
             else:
                 request.pop(i)
-        
+
     # get method and url
     request[0] = request[0].split(' ')
     if len(request[0]) != 2:
@@ -208,13 +211,13 @@ def http_parseRequest(request: list[str]):
         while len(request) > 0 and request[0] != "":
             header = request.pop(0).split(':',1)
             if len(header) != 2:
-                raise ValueError(f"Header need to follow this format:\nheader_name: header_value")
+                raise ValueError("Header need to follow this format:\nheader_name: header_value")
             output.headers[header[0].strip()] = header[1].strip()
 
         # get body
         if len(request) > 1:
             output.body = "\n".join(request[1:])
-        
+
     return output
 
 def http_curl(command: list[str]):
